775e512c27d82649b179acb9a5d33d486a2f53cf
Jon Stern <jonathan.stern@iprospect.com>
2019-12-06 14:43:09 -0600

    day2 part 2

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -10,6 +10,23 @@ export function answer1(): number {
     return computer.run()[0];
 }
 
+export function answer2(): number | undefined {
+    const input = readLines("day2")[0];
+
+    for (let noun = 0; noun < 100; noun++) {
+        for (let verb = 0; verb < 100; verb++) {
+            computer.load(input);
+            computer.data[1] = noun;
+            computer.data[2] = verb;
+            const output = computer.run()[0];
+            if (output === 19690720) {
+                return (100 * noun) + verb;
+            }
+        }
+    }
+}
+
 if (process.argv[1] == __filename) {
     console.log(answer1());
+    console.log(answer2());
 }
